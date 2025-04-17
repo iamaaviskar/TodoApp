a7593d6d05954fe6ec2fb1b50b45808475cceb0c
Raksivaa <kazulaw69@gmail.com>
2025-04-17 08:19:49 +0545

    Removed sever/db.js and updated .gitignore

---
+++ sever/db.js
@@ -0,0 +1,19 @@
+import pg from "pg";
+
+const db = new pg.Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "todoapp",
+  password: "imbadatmaths69",
+  port: 5433,
+});
+
+try {
+  db.connect(() => {
+    console.log("Connected to database!!");
+  });
+} catch (err) {
+  console.error(err.message);
+}
+
+export default db;
